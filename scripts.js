6e5f6d185e8581045e2c143084dff733ae60787c
Katarzyna <kasia.oczkowska@gmail.com>
2019-06-19 08:41:13 +0800

    Refactoring part 2

--- scripts.js
+++ scripts.js
@@ -1,31 +1,123 @@
-var cryptocurrenciesTextEditor = (function() {
+var cryptoCurrenciesTextEditor = (function() {
   const apiEndpoint = "https://api.coinpaprika.com/v1/";
   const textArea = document.getElementById("textArea");
   const convertedText = document.getElementById("convertedText");
-  const debouncedUpdateCurrencyCache = _.debounce(
-    updateCurrencyCache, // could define this function inside and rename the parent to updateCurrencyCache
-    600
-  );
   const currencyCache = [];
-  const availableFunctionNames = ["Name", "Price"];
-  const matches = []; // CONST FFS. also, please move this definition so that it's right below foundCurrencies
-  const functions = {
-    //rename to availableFunctions for consistency
-    updateName: updateName, // consider making this a module with these functions defined within
-    updatePrice: updatePrice
+  const matches = [];
+  const availableMethods = initAvailableMethods();
+  const updateCurrencyCache = initUpdateCurrencyCache();
+
+  return {
+    init: init
   };
+
+  function init() {
+    textArea.oninput = updateOutputText;
+  }
 
   function updateOutputText(event) {
     const value = event.target.value;
-    debouncedUpdateCurrencyCache(value);
+    updateCurrencyCache(value);
     if (currencyCache.length > 0) {
       convertText(value);
     }
   }
 
+  function initUpdateCurrencyCache() {
+    return _.debounce(
+      updateCurrencyCache, // could define this function inside and rename the parent to updateCurrencyCache
+      600
+    );
+
+    function updateCurrencyCache(text) {
+      updateMatches(text);
+      let shouldFetchNewData = false;
+
+      if (matches) {
+        matches.map(match => {
+          const { attributeName, symbol } = getAttributeNameAndSymbolPair(
+            match
+          );
+          const currency = currencyCache.find(currency => {
+            return currency.symbol === symbol;
+          });
+
+          if (currency) {
+            if (!currency[attributeName]) {
+              currency[attributeName] = null;
+              shouldFetchNewData = true;
+            }
+          } else {
+            const newFoundMatch = {
+              ["symbol"]: symbol,
+              [attributeName]: null
+            };
+            currencyCache.push(newFoundMatch);
+            shouldFetchNewData = true;
+          }
+        });
+
+        console.log(currencyCache);
+        if (shouldFetchNewData) {
+          fetchNewDataForCurrencyCache().then(() => {
+            convertText(text);
+          });
+        }
+      }
+    }
+  }
+
+  function initAvailableMethods() {
+    return {
+      Name: updateBasicInfo, // consider making this a module with these functions defined within
+      Price: updatePrice
+    };
+
+    function updateBasicInfo(currency) {
+      return axios
+        .get(
+          `${apiEndpoint}search/?q=${currency.symbol}&modifier=symbol_search`
+        )
+        .then(res => {
+          const currencyData = res.data.currencies.find(item => {
+            return item.symbol === currency.symbol;
+          });
+
+          if (currencyData) {
+            currency.name = currencyData.name;
+            currency.id = currencyData.id;
+          } else {
+            currency.name = "invalid";
+            currency.id = "invalid";
+          }
+        });
+    }
+
+    async function updatePrice(currency) {
+      if (!currency.id) {
+        await updateBasicInfo(currency);
+      }
+
+      if (currency.id !== "invalid") {
+        return axios.get(`${apiEndpoint}tickers/${currency.id}`).then(res => {
+          const price = res.data.quotes.USD.price;
+
+          if (price) {
+            currency.price = price;
+          } else {
+            currency.price = "invalid";
+          }
+        });
+      } else {
+        currency.price = "invalid";
+      }
+    }
+  }
+
   function updateMatches(text) {
-    availableFunctionNames.forEach(functionName => {
-      const regx = new RegExp(`{{ (${functionName})\/\\w+ }}`, "g");
+    const methodNames = Object.keys(availableMethods);
+    methodNames.forEach(methodName => {
+      const regx = new RegExp(`{{ (${methodName})\/\\w+ }}`, "g");
       const newMatches = text.match(regx) || [];
       matches.push(...newMatches);
     });
@@ -39,49 +131,13 @@ var cryptocurrenciesTextEditor = (function() {
     return { attributeName, symbol };
   }
 
-  function updateCurrencyCache(text) {
-    updateMatches(text);
-    let shouldFetchNewData = false;
-
-    if (matches) {
-      matches.map(match => {
-        const { attributeName, symbol } = getAttributeNameAndSymbolPair(match);
-        const currency = currencyCache.find(currency => {
-          return currency.symbol === symbol;
-        });
-
-        if (currency) {
-          if (!currency[attributeName]) {
-            currency[attributeName] = null;
-            shouldFetchNewData = true;
-          }
-        } else {
-          const newFoundMatch = {
-            ["symbol"]: symbol,
-            [attributeName]: null
-          };
-          currencyCache.push(newFoundMatch);
-          shouldFetchNewData = true;
-        }
-      });
-
-      console.log(currencyCache);
-      if (shouldFetchNewData) {
-        fetchNewDataForCurrencyCache().then(() => {
-          convertText(text);
-        });
-      }
-    }
-  }
-
   function fetchNewDataForCurrencyCache() {
     return Promise.all(
       currencyCache.map(async currency => {
         for (var prop in currency) {
           if (!currency[prop]) {
-            const functionName =
-              "update" + prop.charAt(0).toUpperCase() + prop.slice(1); //change to update
-            return await functions[functionName](currency);
+            const methodName = prop.charAt(0).toUpperCase() + prop.slice(1); //change to update
+            await availableMethods[methodName](currency);
           }
         }
       })
@@ -108,53 +164,8 @@ var cryptocurrenciesTextEditor = (function() {
         }
       });
     }
-
     convertedText.innerText = textWithReplacedMatches;
   }
-
-  function updateName(currency) {
-    return axios
-      .get(`${apiEndpoint}search/?q=${currency.symbol}&modifier=symbol_search`)
-      .then(res => {
-        const currencyData = res.data.currencies.find(item => {
-          return item.symbol === currency.symbol;
-        });
-
-        if (currencyData) {
-          currency.name = currencyData.name;
-          currency.id = currencyData.id;
-        } else {
-          currency.name = "invalid";
-          currency.id = "invalid";
-        }
-      });
-  }
-
-  async function updatePrice(currency) {
-    if (!currency.id) {
-      await updateName(currency);
-    }
-
-    if (currency.id !== "invalid") {
-      return axios.get(`${apiEndpoint}tickers/${currency.id}`).then(res => {
-        const price = res.data.quotes.USD.price;
-
-        if (price) {
-          currency.price = price;
-        } else {
-          currency.price = "invalid";
-        }
-      });
-    }
-  }
-
-  function init() {
-    textArea.oninput = updateOutputText;
-  }
-
-  return {
-    init: init
-  };
 })();
 
-cryptocurrenciesTextEditor.init();
+cryptoCurrenciesTextEditor.init();
